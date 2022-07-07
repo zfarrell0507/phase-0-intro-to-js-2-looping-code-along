841a64bf0036a9da0d4b28c21eb526ec09d38326
Zachary Farrell <zfarrell0507@gmail.com>
2022-07-06 20:06:54 -0400

    commit tree 0b0bb13dc7799fa2cfcee4c290d884944b654c3b

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solutions in this file
+  
+  const name = ["Guadalupe", "Ollie", "Aki"];
+  const event = ("surprise");
+  const cards = [];
+  function writeCards(name) {
+    for (let i = 0; i < name.length; i++) {
+        let writing = (`Thank you, ${name[i]}, for the wonderful ${event} gift!`);
+        cards.push(writing);
+    }
+
+    return cards;
+  }
+
+  function countDown() {
+  let count = 10;
+  while (count >= 0) {
+    console.log(count);
+    count -= 1;
+  }
+    return count;
+  }
